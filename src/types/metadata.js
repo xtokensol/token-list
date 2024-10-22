29a62ea0be6b47f851d6c423a15510d96fa6e6cc
xtokensol <batosojesoro489489@gmail.com>
2024-10-22 21:27:46 +0300

    Add files via upload

---
+++ src/types/metadata.js
@@ -0,0 +1,6 @@
+{
+"name": "TrumpAPE",
+"symbol": "TAPE",
+"description": "Exploring the world of crypto with TAPE on the Solana blockchain.",
+"image": "https://raw.githubusercontent.com/xtokensol/token-list/refs/heads/main/src/TAPE.png"
+}
